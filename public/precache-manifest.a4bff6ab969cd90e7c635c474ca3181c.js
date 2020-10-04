621be991d3a9f8891d1631783e775e01d64eadcb
mihaidld <mihaidoldur@gmail.com>
2020-10-04 19:05:44 +0200

    added react app with login request to express server

---
+++ public/precache-manifest.a4bff6ab969cd90e7c635c474ca3181c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac3da59efde5ea5ecc7cbb3bb0795f06",
+    "url": "/index.html"
+  },
+  {
+    "revision": "570a9d45c13fd024e58b",
+    "url": "/static/css/2.4fa49d0a.chunk.css"
+  },
+  {
+    "revision": "570a9d45c13fd024e58b",
+    "url": "/static/js/2.6cfdea22.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.6cfdea22.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d52e174dea56f06a3a5",
+    "url": "/static/js/main.8f1cd46d.chunk.js"
+  },
+  {
+    "revision": "b5ffb88fa2abd1db339d",
+    "url": "/static/js/runtime-main.1bb48197.js"
+  }
+]);
